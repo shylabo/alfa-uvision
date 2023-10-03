064a92b82e85d98e53137a08b3a875514f7cecad
shylabo <shuya.sohot@gmail.com>
2023-10-03 13:49:48 -0700

    update: mui setup

--- app/studio/components/MainContentWrapper.tsx
+++
@@ -1,8 +0,0 @@
-interface MainWrapperProps {
-  children: React.ReactNode
-}
-const MainWrapper: React.FC<MainWrapperProps> = ({ children }) => {
-  return <main className="pt-14 pl-60">{children}</main>
-}
-
-export default MainWrapper
--- components/Container.tsx
+++
@@ -1,9 +0,0 @@
-interface ContainerProps {
-  children: React.ReactNode
-}
-
-const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div className="container mx-auto h-screen grid grid-cols-1">{children}</div>
-}
-
-export default Container
